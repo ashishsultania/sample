d3059318fd2608b1aaf9ae08f8ef070b9a1be940
ashish <ashishsultania2k7@gmail.com>
2017-07-15 13:22:14 +0300

    Views changed

--- WebApp/config/database.js
+++ WebApp/config/database.js
@@ -1,11 +1,7 @@
 // config/database.js
 module.exports = {
 
-    'ooblibPath' : '.',
     'dbPath' : 'peer_connection_db',
     'url'    : '',
     'basedir' : '/home/ashish/sample/',
-    'enableAccessControl' : '0', //set '1' to enable
-    'OobRetries' : '5' //number of times to try sending OOB before sending failure to peer
-
 };
--- WebApp/config/passport.js
+++ WebApp/config/passport.js
@@ -1,13 +1,9 @@
-// config/passport.js
-
-// load all the things we need
 var LocalStrategy   = require('passport-local').Strategy;
 var sqlite3 		= require('sqlite3').verbose();
 var bcrypt 			= require('bcrypt-nodejs');
 
 // load up the user model
 var db;
-
 var configDB = require('./database.js');
 var conn_str = configDB.dbPath;
 
@@ -20,46 +16,42 @@ function validPassword(password,original_password){
 	return bcrypt.compareSync(password, original_password);
 }
 
-// expose this function to our app using module.exports
 module.exports = function(passport) {
 
-	// =========================================================================
-	// passport session setup ==================================================
-	// =========================================================================
-	// required for persistent login sessions
-	// passport needs ability to serialize and unserialize users out of session
+	// =======================================
+	// passport session setup ================
+	// =======================================
+	// required for persistent login sessions	
 
-	// used to serialize the user for the session
-	passport.serializeUser(function(user, done) {
-			done(null, user.id);
-			});
+	passport.serializeUser(function(user, done)
+    {
+		done(null, user.id);
+	});
 
 	// used to deserialize the user
-	passport.deserializeUser(function(id, done) {
-			db = new sqlite3.Database(conn_str);
-			db.get('SELECT id, username, password, isAdmin FROM users WHERE id = ?', id, function(err, row) {
+	passport.deserializeUser(function(id, done) 
+	{
+		db = new sqlite3.Database(conn_str);
+		db.get('SELECT id, username, password, isAdmin FROM users WHERE id = ?', id, function(err, row) {
 
-				db.close();
-				if (!row) return done(null, false);
-				return done(null, row);
-				});
-			});
-	// =========================================================================
-	// LOCAL LOGIN =============================================================
-	// =========================================================================
-	// we are using named strategies since we have one for login and one for signup
-	// by default, if there was no name, it would just be called 'local'
+			db.close();
+			if (!row) return done(null, false);
+			return done(null, row);
+		});
+	});
+	// ========================================
+	// LOCAL LOGIN ============================
+	// ========================================
+	// Using named strategies since we have one for login and one for signup
 
 	passport.use('local-login', new LocalStrategy({
-				// by default, local strategy uses username and password, we will override with email
 		usernameField : 'email',
 		passwordField : 'password',
-		passReqToCallback : true // allows us to pass back the entire request to the callback
+		passReqToCallback : true // allows to pass back the entire request to the callback
 		},
 		function(req, email, password, done) { // callback with email and password from our form
 		
-		// find a user whose email is the same as the forms email
-		// we are checking to see if the user trying to login already exists
+		// Checking if the user trying to login already exists
 		db = new sqlite3.Database(conn_str);
 		db.get('SELECT id, username, password, isAdmin FROM users WHERE username = ?', email, function(err, row) {
 
@@ -67,7 +59,7 @@ module.exports = function(passport) {
 		if(err) return done(err);
 		// if no user is found, return the message
 		if (!row){
-		return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
+			return done(null, false, req.flash('loginMessage', 'No user found.')); // req.flash is the way to set flashdata using connect-flash
 		}
 		// if the user is found but the password is wrong
 		if (!validPassword(password,row.password))
@@ -79,14 +71,11 @@ module.exports = function(passport) {
 }));
 
 
-	// =========================================================================
-	// LOCAL SIGNUP ============================================================
-	// =========================================================================
-	// we are using named strategies since we have one for login and one for signup
-	// by default, if there was no name, it would just be called 'local'
+	// ======================================
+	// LOCAL SIGNUP =========================
+	// ======================================
 
 	passport.use('local-signup', new LocalStrategy({
-			// by default, local strategy uses username and password, we will override with email
 	usernameField : 'email',
 	passwordField : 'password',
 	passReqToCallback : true // allows us to pass back the entire request to the callback
@@ -95,30 +84,31 @@ module.exports = function(passport) {
 	
 	// asynchronous
 	// User.findOne wont fire unless data is sent back
-	process.nextTick(function() {
+	process.nextTick(function() 
+	{
+		// find a user whose email is the same as the forms email
+		db = new sqlite3.Database(conn_str);
+		db.get('SELECT id FROM users WHERE username = ?', email, function(err, row) 
+		{
 	
-	// find a user whose email is the same as the forms email
-	// we are checking to see if the user trying to login already exists
-	db = new sqlite3.Database(conn_str);
-	db.get('SELECT id FROM users WHERE username = ?', email, function(err, row) {
-
-		db.close();
-		if(err) return done(err);
-		if(row){	
-		return done(null, false, req.flash('signupMessage', 'That email is already taken.'));
-		}
-		if (!row){
-		var stmt = db.prepare("INSERT INTO users(username,password) VALUES(?,?)");
-		stmt.run(email,hashPassword(password));
-		stmt.finalize();
-		db.get('SELECT id, username, password, isAdmin FROM users WHERE username = ?', email, function(err, row) {
-			if (!row) return done(null, false);
-			else return done(null,row);
-			});
-		}
+			db.close();
+			if(err) return done(err);
+			if(row){	
+				return done(null, false, req.flash('signupMessage', 'The provided email already used.'));
+			}
+			if (!row)
+			{
+				var stmt = db.prepare("INSERT INTO users(username,password) VALUES(?,?)");
+				stmt.run(email,hashPassword(password));
+				stmt.finalize();
+				db.get('SELECT id, username, password, isAdmin FROM users WHERE username = ?', email, function(err, row) 
+				{
+					if (!row) return done(null, false);
+					else return done(null,row);
+				});
+			}
 		});    
-
-});
+	});
 
 }));
 
